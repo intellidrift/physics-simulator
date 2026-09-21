1312b976f91916295979c57275c5b4d3dbbfde1b
binentity <binentity@yandex.ru>
2026-09-21 14:10:52 +0300

    Добавьте файлы проекта.

---
+++ Core/Application.cs
@@ -0,0 +1,33 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicsSimulator.Core {
+
+    internal class Application {
+
+        private static readonly string title = "PhysicsSimulator";
+
+        private VideoMode mode;
+        private RenderWindow window;
+
+        public Application() {
+            mode = new(new Vector2u(2550, 1440));
+            window = new(mode, title);
+            Console.WriteLine("hello world");
+        }
+
+        public void Run() {
+            while (window.IsOpen) {
+                window.DispatchEvents();
+                
+                window.Clear();
+                window.Display();
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using PhysicsSimulator.Core;
+
+namespace PhysicsSimulator {
+    internal class Program {
+        static void Main(string[] args) {
+            new Application().Run();
+        }
+    }
+}
