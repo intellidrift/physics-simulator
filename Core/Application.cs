748894304666e77ca4aea3996326173d10a4dc9c
binentity <binentity@yandex.ru>
2026-09-22 17:53:34 +0300

    Make an app skeleton.

--- Core/Application.cs
+++ Core/Application.cs
@@ -10,24 +10,70 @@ namespace PhysicsSimulator.Core {
     internal class Application {
 
         private static readonly string title = "PhysicsSimulator";
+        private static readonly uint width = 2550;
+        private static readonly uint height = 1440;
 
         private VideoMode mode;
-        private RenderWindow window;
+        private readonly RenderWindow window;
+        private readonly Clock timer;
+
+        private float dt;
+        private List<CircleShape> objects;
 
         public Application() {
-            mode = new(new Vector2u(2550, 1440));
+            mode = new(new Vector2u(width, height));
             window = new(mode, title);
-            Console.WriteLine("hello world");
+            timer = new();
+            dt = 0;
+            
+            window.Closed += (sender, args) => window.Close();
+            window.KeyPressed += WindowKeyPressed;
+
+            objects = [
+                new CircleShape(50) {
+                    Position = new Vector2f(100, 100),
+                    FillColor = Color.Yellow
+                },
+                new CircleShape(35) {
+                    Position = new Vector2f(500, 500),
+                    FillColor = Color.Yellow
+                }
+            ];
+        }
+
+        private void WindowKeyPressed(object? sender, KeyEventArgs e) {
+            if (e.Code == Keyboard.Key.Enter) {
+                window.Close();
+            }
         }
 
         public void Run() {
+
             while (window.IsOpen) {
+                TickApp();
+
+                // Event dispatching
                 window.DispatchEvents();
                 
-                window.Clear();
+                // TODO: Update State
+
+                // Drawing
+                window.Clear(Color.Blue);
+                
+                foreach (var obj in objects) {
+                    window.Draw(obj);
+                }
+
                 window.Display();
             }
         }
-        
+
+        private void TickApp() {
+            dt = timer.Restart().AsSeconds();
+            if (dt > 0.1) {
+                dt = 0;
+            }
+        }
+
     }
 }
